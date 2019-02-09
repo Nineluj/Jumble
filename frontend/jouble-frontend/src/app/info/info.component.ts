6e73547dca28633c35ab6bd0553fa1f4cc607296
thomas-vaiarella <vaiarella.t@husky.neu.edu>
2019-02-09 12:33:08 -0500

    Added a CurrentCardDataService

--- frontend/jouble-frontend/src/app/info/info.component.ts
+++ frontend/jouble-frontend/src/app/info/info.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SampleData, User } from 'app/userService';
+import { CurrentCardDataService } from 'app/services/current-card-data.service';
 
 @Component({
   selector: 'app-info',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InfoComponent implements OnInit {
 
-  constructor() { }
+  private name: string;
+  private email: string;
+  private major: string;
+  private slack: string;
+  
+  private contacts: User[];
+  constructor(private currentCardData: CurrentCardDataService) { 
+    this.contacts = new SampleData().getContacts();
+  }
 
   ngOnInit() {
+    this.name = this.currentCardData.getFirstName();//this.contacts[0].firstName;
+    this.email = this.currentCardData.getEmail();//"email@email.com";
+    this.major = this.currentCardData.getMajor();//"CE - TEST";
+    this.slack = this.currentCardData.getSlack();//this.contacts[0].slackUsername;
   }
 
 }
